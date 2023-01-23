5a1c5eaaea11dcbc55f8d2d99a15dab6d989464a
Jani Hartikainen <jani.hartikainen@tuni.fi>
2023-01-23 22:47:10 +0200

    replace 'violation(s)' with the right inflection

--- Frontend/src/components/PilotElement.js
+++ Frontend/src/components/PilotElement.js
@@ -1,10 +1,13 @@
 // Handles single pilot as list element
 
 const Pilot = (pilot) => {
+  let violationLabel = pilot.pilot.violations.length === 1 
+  ? "violation" : "violations"
+  
     return (
       <li className="pilot">
         Pilot: {pilot.pilot.firstName} {pilot.pilot.lastName} ({pilot.pilot.phoneNumber}, {pilot.pilot.email}) <br/>
-        Closest distance from nest: {(pilot.pilot.distance/1000).toFixed(2)} meters from {pilot.pilot.violations.length} violation(s)
+        Closest distance from nest: {(pilot.pilot.distance/1000).toFixed(2)} meters from {pilot.pilot.violations.length} {violationLabel}
       </li>
     )
   }
